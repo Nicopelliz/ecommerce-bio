8c279d12cc0817486ed30fa94d8401145824b43a
Niccolò Pellizzari <nicopelliz@gmail.com>
2021-01-22 12:11:40 +0100

    setup_gitignore_and_.env

--- food_commerce/settings.py
+++ food_commerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&(akp#su(ge5f7xf3g1$c60%axed7xjk1z1&6alb0lcp10rw%!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
